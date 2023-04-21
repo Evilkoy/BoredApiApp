08240de15b3bf70dfcee24919559b6108708dd06
Evilkoy <drobkill@gmail.com>
2023-04-21 19:46:59 +0300

    added CORS

--- src/main/java/com/makson/BoredBase/controller/BoredController.java
+++ src/main/java/com/makson/BoredBase/controller/BoredController.java
@@ -5,6 +5,7 @@ import com.makson.BoredBase.service.BoredService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +27,8 @@ public class BoredController {
                 .collect(Collectors.toList());
         return new ResponseEntity<>(activities, HttpStatus.OK);
     }
-
     @GetMapping("/get")
+    @CrossOrigin
     public ResponseEntity<String> getBoredDto() {
         return boredService.getBoredDto().map(BoredDto::getActivity)
                 .map(activity -> new ResponseEntity<>(activity, HttpStatus.OK))
